34bf87728ea67bf3003758487c15e43106d72d12
Otávio <otaviosenun@gmail.com>
2023-04-24 18:10:46 +0100

    Update model

--- purple_rain.py
+++ purple_rain.py
@@ -1,8 +1,5 @@
 import sys
 if __name__ == '__main__':
-    input = sys.argv[1:][0]
-    fstLayer = [float(x)*1 for x in input.split(',')]
-    sndLayer = [.5,.5] # orange, purple
-    orange = fstLayer[0]*sndLayer[0] + fstLayer[1]*sndLayer[0]
-    purple = fstLayer[0]*sndLayer[1] + fstLayer[2]*sndLayer[1]
-    print("purple" if purple > orange else "orange")
+    input = [float(x)*1 for x in sys.argv[1:][0].replace(" ", "").split(',')] # split each value into neural input layer (r,g,b)
+    out = [sum([i*w for i,w in zip(input, weights)]) for weights in [[.5,.5,.0],[.5,.0,.5]]] # propagate values applying weights 
+    print("purple" if out[1] > out[0] else "orange")
